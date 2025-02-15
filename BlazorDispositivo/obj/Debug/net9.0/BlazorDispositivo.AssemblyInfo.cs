eb3e05c967906ed2b97a82093c93ca24bd339b40
Vitor Hugo Sodré Quinelato <vitorhugoquinelato@gmail.com>
2025-02-15 14:53:02 -0400

    criando as paginas

--- BlazorDispositivo/obj/Debug/net9.0/BlazorDispositivo.AssemblyInfo.cs
+++ BlazorDispositivo/obj/Debug/net9.0/BlazorDispositivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorDispositivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ecdd5d05d7e892fdd21a2f3399c45c68fbec746")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorDispositivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorDispositivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
